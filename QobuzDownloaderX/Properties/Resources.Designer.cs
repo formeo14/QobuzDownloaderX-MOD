0068dfe41987b4fd7deaa08a5ffed34d0ce95937
DJDoubleD <34967020+DJDoubleD@users.noreply.github.com>
2024-10-12 00:25:07 +0200

    Add a settings icon

--- QobuzDownloaderX/Properties/Resources.Designer.cs
+++ QobuzDownloaderX/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace QobuzDownloaderX.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap settings_icon {
+            get {
+                object obj = ResourceManager.GetObject("settings_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap smr_audio {
             get {
                 object obj = ResourceManager.GetObject("smr_audio", resourceCulture);
